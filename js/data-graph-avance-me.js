073e7f193bef6750531df9d300ee2df96290dd56
Bartolomé Abramovich M <babramovichm@gmail.com>
2016-11-03 13:20:50 -0300

    Update data-graph-avance-me.js

--- js/data-graph-avance-me.js
+++ js/data-graph-avance-me.js
@@ -6,22 +6,22 @@ long_short_data_me = [
       { 
         "label" : "Normatividad" ,
         "n_palabras" : 2160 ,
-        "value" : 0.46
+        "value" : 0.50
       } ,
       { 
         "label" : "Labor del Congreso" ,
         "n_palabras" : 1679 ,
-        "value" : 0.30
+        "value" : 0.46
       } ,
       { 
         "label" : "Presupuesto y Gestión" ,
         "n_palabras" : 904 ,
-        "value" : 0.49
+        "value" : 0.21
       } ,
       { 
         "label" : "Participación Ciudadana" ,
         "n_palabras" : 882 ,
-        "value" : 0.35
+        "value" : 0.33
       }
     ]
   },
@@ -32,22 +32,22 @@ long_short_data_me = [
       { 
         "label" : "Normatividad" ,
         "n_palabras" : 1246 ,
-        "value" : 0.57
+        "value" : 0.79
       },
       { 
         "label" : "Labor del Congreso" ,
         "n_palabras" : 861 ,
-        "value" : 0.24
+        "value" : 0.41
       },
       { 
         "label" : "Presupuesto y Gestión" ,
         "n_palabras" : 42 ,
-        "value" : 0.49
+        "value" : 0.44
       },
       { 
         "label" : "Participación Ciudadana" ,
         "n_palabras" : 1178 ,
-        "value" : 0.46
+        "value" : 0.57
       }
     ]
   }
