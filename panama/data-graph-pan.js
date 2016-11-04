a957b8a0362083789e91e86078381c2ec4c1605f
Bartolomé Abramovich M <babramovichm@gmail.com>
2016-11-04 11:45:51 -0300

    Actualización datos PY

--- panama/data-graph-pan.js
+++ panama/data-graph-pan.js
@@ -6,7 +6,7 @@ long_short_data = [
       { 
         "label" : "Panamá" ,
         "n_palabras" : 35 ,
-        "value" : 0.54
+        "value" : 0.45
       }
     ]
   },
@@ -17,7 +17,7 @@ long_short_data = [
       { 
         "label" : "Panamá" ,
         "n_palabras" : 46 ,
-        "value" : 0.55
+        "value" : 0.38
       }
     ]
   },
@@ -28,7 +28,7 @@ long_short_data = [
       { 
         "label" : "Panamá" ,
         "n_palabras" : 25 ,
-        "value" : 0.55
+        "value" : 0.36
       }
     ]
   },
@@ -39,7 +39,7 @@ long_short_data = [
       { 
         "label" : "Panamá" ,
         "n_palabras" : 45 ,
-        "value" : 0.52
+        "value" : 0.58
       },
     ]
   }
