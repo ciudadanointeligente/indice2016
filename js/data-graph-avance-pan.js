29fb9d271370dd88ac9e6b2ad26748a4c1f53031
Manuel Barros R <mbarros@ciudadanointeligente.org>
2016-11-04 11:50:49 -0400

    Actualiza datos

--- js/data-graph-avance-pan.js
+++ js/data-graph-avance-pan.js
@@ -6,22 +6,22 @@ long_short_data_pan = [
       { 
         "label" : "Normatividad" ,
         "n_palabras" : 1246 ,
-        "value" : 0.57
+        "value" : 0.45
       },
       { 
         "label" : "Labor del Congreso" ,
         "n_palabras" : 861 ,
-        "value" : 0.53
+        "value" : 0.38
       },
       { 
         "label" : "Presupuesto y Gestión" ,
         "n_palabras" : 42 ,
-        "value" : 0.27
+        "value" : 0.36
       },
       { 
         "label" : "Participación Ciudadana" ,
         "n_palabras" : 1178 ,
-        "value" : 0.32
+        "value" : 0.58
       }
     ]
   }
